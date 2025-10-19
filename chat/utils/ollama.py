1dad4f79287b8adc80c184316a349f212c9f0fff
NikaShabashvili <104011629+NikaShabashvili03@users.noreply.github.com>
2025-10-19 23:12:39 +0400

    sd

--- chat/utils/ollama.py
+++ chat/utils/ollama.py
@@ -1,12 +1,12 @@
-from ..serializers import MessageSerializer, MessageCreateSerializer
-from ..models import Message, Conversation, Assistant
+# chat/ollama_client.py
 import subprocess
 
-OLLAMA_MODEL = "llama2-mini"
-OLLAMA_PATH = "/snap/bin/ollama"
+OLLAMA_MODEL = "qwen3-0.6b"
+OLLAMA_PATH = "/snap/bin/ollama" 
 
 def ask_ollama(prompt: str) -> str:
     try:
+
         process = subprocess.Popen(
             [OLLAMA_PATH, "run", OLLAMA_MODEL],
             stdin=subprocess.PIPE,
@@ -14,9 +14,13 @@ def ask_ollama(prompt: str) -> str:
             stderr=subprocess.PIPE,
             text=True
         )
-        stdout, stderr = process.communicate(prompt + "\n")  # add newline
+
+
+        stdout, stderr = process.communicate(prompt)
+
         if process.returncode != 0:
             return f"Ollama error: {stderr.strip()}"
+
         return stdout.strip()
     except Exception as e:
         return f"Ollama exception: {e}"
